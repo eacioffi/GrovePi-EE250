2c5cbeb4817b12b0fb0d587f8ce1bfa6b32527ed
Eric Cioffi <ecioffi@usc.edu>
2018-02-20 16:36:43 -0800

    tring for part3

--- ee250-lab04/part3/ultrasonicClient.py
+++ ee250-lab04/part3/ultrasonicClient.py
@@ -10,3 +10,36 @@ import sys
 sys.path.append('../../Software/Python/')
 
 import grovepi
+import socket
+
+def Main():
+    # Change the host and port as needed. For ports, use a number in the 9000 
+    # range. 
+    host = '127.0.0.1'
+    port = 9000
+
+    server_addr = '127.0.0.1'
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    s.bind((host,port))
+
+    # UDP is connectionless, so a client does not formally connect to a server
+    # before sending a message.
+    dst_port = 9000
+    while True:
+        #tuples are immutable so we need to overwrite the last tuple
+        server = (server_addr, int(dst_port))
+
+        # for UDP, sendto() and recvfrom() are used instead
+		try:
+        # Read distance value from Ultrasonic
+        	data = grovepi.ultrasonicRead(ultrasonic_ranger)
+        	s.sendto(data.encode('utf-8'), server) 
+        except TypeError:
+        	print ("Error")
+    	except IOError:
+        	print ("Error")
+    s.close()
+
+if __name__ == '__main__':
+    Main()
--- ee250-lab04/part3/ultrasonicServer.py
+++ ee250-lab04/part3/ultrasonicServer.py
@@ -2,3 +2,42 @@
 #
 # This code runs on your VM and receives a stream of packets holding ultrasonic
 # sensor data and prints it to stdout. Use a UDP socket here.
+
+import grovepi
+import socket
+
+    
+
+# Connect the Grove Ultrasonic Ranger to digital port D4
+# SIG,NC,VCC,GND
+ultrasonic_ranger = 4
+
+def Process2():
+    # Change the host and port as needed. For ports, use a number in the 9000 
+    # range. 
+    host = '127.0.0.1'
+    port = 9000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host,port))
+
+    print("VM Server Started")
+    while True:
+
+    try:
+        # Read distance value from Ultrasonic
+        sensorData = grovepi.ultrasonicRead(ultrasonic_ranger)
+
+    except TypeError:
+        print ("Error")
+    except IOError:
+        print ("Error")
+
+        data, addr = s.recvfrom(1024)
+        data = data.decode('utf-8')
+        print("Message From: " + str(addr))
+        print("From connected pi: " + data)
+    c.close()
+
+if __name__ == '__main__':
+    Process2()
